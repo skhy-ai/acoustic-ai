361ad5101f4448d3da73e1a848969e73ccc78b1a
Babu Priyavratt <babu.priyavrat@gmail.com>
2026-02-13 11:03:15 +0800

    chore: Update Uvicorn run command to explicitly reference the application module.

--- sound_classifier_system/api/main.py
+++ sound_classifier_system/api/main.py
@@ -30,4 +30,4 @@ async def root():
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("api.main:app", host="0.0.0.0", port=8000, reload=True)
